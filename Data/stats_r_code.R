46845179363ec987b3f5991be387844240c2d02d
Jade Meneghini <jade,meneghini@outlook.com>
2024-02-19 10:04:19 +0100

    Project setup

---
+++ Data/stats_r_code.R
@@ -0,0 +1,7 @@
+#Select the local working directory
+#proj_directory <- "C:/Users/jadem/OneDrive/Desktop/BABD/Statistics/PersoProject/Statistics_Project/Data"
+proj_directory <- "C:/Users/jadem/OneDrive/Desktop/BABD/Statistics/PersoProject/Statistics_Project/Data"
+setwd(proj_directory)
+
+#Import data from local .csv
+data <- read.csv("cost-of-living_v2.csv")
